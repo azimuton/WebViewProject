8af73b239744c11f2faa09fe5431d85d53d65ff5
ArtemZonov <artemzonov21@gmail.com>
2023-12-05 14:11:03 +0200

    init

---
+++ app/src/main/java/com/azimuton/webviewproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.azimuton.webviewproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
